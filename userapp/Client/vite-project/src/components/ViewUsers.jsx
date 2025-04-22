c50fe1a50a5ee6660564003e62ac49d452575ef8
Anujmishra2005 <anujmishraan2005@gmail.com>
2025-04-22 19:17:40 +0530

    added cors

--- userapp/Client/vite-project/src/components/ViewUsers.jsx
+++ userapp/Client/vite-project/src/components/ViewUsers.jsx
@@ -1,53 +1,57 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const ViewUsers = () => {
-    const [users,setUsers] = useState([]);
-    const [error,setError] = useState(null);
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState(null);
 
-    useEffect(()=>{
-        fetchUsers();    
+    useEffect(() => {
+        fetchUsers();
+    }, []);
 
-    },[])
-
-    const fetchUsers=async()=>{
-        try{
+    const fetchUsers = async () => {
+        try {
             const res = await axios.get("https://userapp6.onrender.com/users");
             setUsers(res.data);
-        }
-        catch(err){
-            console.log("users Fetching Error",err.message);
+        } catch (err) {
+            console.error("Users Fetching Error", err.message);
             setError(err.message);
         }
-    }
-  return (
-    <div className='content'>
-        <h1>List of Users :- </h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Sr.No</th>
-                    <th>User Name</th>
-                    <th>User Email</th>
-                    <th>User Role</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {error || users.map((users,index)=>{
-                    <tr>
-                        <td>{++index}</td>
-                        <td>{user.name}</td>
-                        <td>{user.email}</td>
-                        <td>{user.role}</td>
-                        <td>
-                            <button>Edit</button>
-                        </td>
-                    </tr>
-                })}
-            </tbody>
-        </table>
-    </div>
-  )
-}
+    };
 
-export default ViewUsers
+    return (
+        <div className='content'>
+            <h1>List of Users:</h1>
+            {error ? (
+                <p style={{ color: 'red' }}>Error: {error}</p>
+            ) : (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Sr.No</th>
+                            <th>User Name</th>
+                            <th>User Email</th>
+                            <th>User Role</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user, index) => (
+                            <tr key={user._id || index}>
+                                <td>{index + 1}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.role}</td>
+                                <td>
+                                    <button>Edit</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </div>
+    );
+};
+
+export default ViewUsers;
